4fc44b6d5e4012ed3315afa4b3b037aa784f9261
Secchinho <raphasec@gmail.com>
2025-06-28 12:31:23 -0300

    rascunho leitura arquivo

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "fila.h"
+#define CANAIS 100
 
 int main(int argc, char const *argv[])
 {
@@ -11,8 +13,13 @@ int main(int argc, char const *argv[])
 
     FILE *arq = fopen(argv[1], "r");
 
-    int canAtual = 0, canDestino = 0, qtdAdulto = 0;
-    int canAdulto[101];
+    int canAtual = 0, canTv = 0, qtdAdulto = 0;
+    bool canAdulto[CANAIS] = {false};
 
+    //Lendo a primeira linha do arquivo
+
+    fscanf(arq, "%d %d %d", &canAtual, &canTv, &qtdAdulto);
+
+    fclose(arq);
     return 0;
 }
